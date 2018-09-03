be565ea4330e9fc3db5d38964502e2c8920b314f
saruatchu <sarumathiayyappan@gmail.com>
2018-09-03 12:25:16 +0530

    Create Numbersk.java

---
+++ Numbersk.java
@@ -0,0 +1,25 @@
+# Numbersk
+import java.io.*;
+import java.util.*;
+class Numbersk
+{
+public static void amin(String args[])
+{
+Scanner in=new Scanner(System.in);
+Scanner.out.println("input");
+int N=in.nextInt();
+int K=in.nexttInt();
+int sum=0;
+int a[0]=new int[N];
+for(int i=0;i<=a.length;i++)
+{
+a[i]=in.nextInt();
+}
+for(int i=0;i<k;i++)
+{
+sum=sum+a[i];
+}
+System.out.println("output");
+System.out.println(sum);
+}
+}
